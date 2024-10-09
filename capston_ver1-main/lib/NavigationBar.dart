af78ae4223801a81f6b61d1131b3723cb65f3715
Lene9029 <seanharoldrebodos@gmail.com>
2024-10-09 22:07:29 +0800

    update

--- capston_ver1-main/lib/NavigationBar.dart
+++ capston_ver1-main/lib/NavigationBar.dart
@@ -4,36 +4,41 @@ import 'package:recipe_page_new/Detect_Object_Page.dart';
 import 'package:recipe_page_new/profile_page.dart';
 import 'package:recipe_page_new/ui/screens/all_recipe_screen.dart';
 import 'package:recipe_page_new/ui/screens/favorite_recipes_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final controller = Get.put(NavigationController());
 
     return Scaffold(
       bottomNavigationBar: Obx(
-        () =>  NavigationBar(
-          height: 60,
-          elevation: 5,
-          selectedIndex: controller.selectedIndex.value,
-          onDestinationSelected: (index) => controller.selectedIndex.value = index,
-          destinations: const [ 
-            NavigationDestination(icon: Icon(Icons.restaurant_menu), label: 'Recipes'),
-            NavigationDestination(icon: Icon(Icons.camera), label: 'Camera'),
-            NavigationDestination(icon: Icon(Icons.favorite), label: 'Favorites'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
-          ],),
+        () => CurvedNavigationBar(
+          height: 50,
+          items: const <Widget>[
+            Icon(Icons.restaurant_menu, size: 30),
+            Icon(Icons.camera, size: 30),
+            Icon(Icons.favorite, size: 30),
+            Icon(Icons.person, size: 30),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.blue,
+          backgroundColor: Colors.grey[200]!, 
+          animationCurve: Curves.easeInOut,
+          animationDuration: const Duration(milliseconds: 500),
+          onTap: (index) => controller.selectedIndex.value = index,
+          index: controller.selectedIndex.value,
+        ),
       ),
-        body: Obx (() => controller.screens[controller.selectedIndex.value]),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
 }
 
-class NavigationController extends GetxController{
-  final Rx<int> selectedIndex = 1.obs;
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 1.obs; 
 
   final screens = [
     const MainRecipeScreen(),
